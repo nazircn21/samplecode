d39e640f9574325af9050d7b15b8f4ffb5231b97
nazircn21 <94355338+nazircn21@users.noreply.github.com>
2021-11-17 16:15:01 +0530

    Add files via upload

---
+++ dot Net Code Sample/WebApplication1/Data/WebApplication1Context.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Models;
+
+namespace WebApplication1.Data
+{
+    public class WebApplication1Context : DbContext
+    {
+       
+
+        public WebApplication1Context (DbContextOptions<WebApplication1Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebApplication1.Models.EmployeeModel> EmployeeModel { get; set; }
+        public DbSet<WebApplication1.Models.ErrorLog> ErrorLogs { get; set; }
+        public DbSet<WebApplication1.Models.Department> Department { get; set; }
+    }
+}
